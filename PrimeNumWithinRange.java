9a079e3793c7151be46e4f956409d87db0825c80
Shashi raj <shashiraj13457@gmail.com>
2024-12-13 20:49:24 +0530

    Prime in range

--- PrimeNumWithinRange.java
+++ PrimeNumWithinRange.java
@@ -1,4 +1,26 @@
 package Codes;
-
+import java.util.Scanner;
 public class PrimeNumWithinRange {
+    public static boolean isPrime(int n){
+        if(n < 2){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int lower = sc.nextInt();
+        int upper = sc.nextInt();
+
+        for (int i = lower; i <= upper; i++) {
+            if(isPrime(i)){
+                System.out.println(i);
+            }
+        }
+    }
 }
