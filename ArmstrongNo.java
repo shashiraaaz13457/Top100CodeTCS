d203e5d1a9d0abf7901691c49d02fa0be95c8604
Shashi raj <shashiraj13457@gmail.com>
2024-12-24 19:07:08 +0530

    Armstrong Number

--- ArmstrongNo.java
+++ ArmstrongNo.java
@@ -1,7 +1,27 @@
 package Codes;
 
+import java.util.*;
+
 public class ArmstrongNo {
+    public static boolean ArmstrongCheck(int number){
+        int temp = number;
+        int numberOfDigit = (int) Math.log10(number)+1;
+        int sum = 0;
+        while (number != 0){
+            int rem = number % 10;
+//            sum = sum + rem * rem * rem; // for 3-digit only
+            sum += Math.pow(rem,numberOfDigit);
+            number = number/10;
+        }
+
+        return sum == temp;
+
+    }
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+
+        boolean result = ArmstrongCheck(number);
+        System.out.println(result);
     }
 }
